ce14eb02d5c948a39abb2cb6f3df1362724b1bb7
HenricoHosaki <126363997+HenricoHosaki@users.noreply.github.com>
2024-10-25 20:24:24 -0300

    Update PainelDoJogo.java

--- PainelDoJogo.java
+++ PainelDoJogo.java
@@ -1,1 +1,23 @@
 
+import javax.swing.JFrame;
+
+class JanelaDoJogo extends JFrame {
+
+    public JanelaDoJogo() {
+        janela();
+    }
+
+    public void janela() {
+        this.setTitle("Volleyball");
+        this.setSize(400, 600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.add(new PainelDoJogo());
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new JanelaDoJogo();
+
+    }
+}
+
